c9d9e361aca971c3a713aafa68cb75b485299eed
Ayush-G <totallynotayush@outlook.com>
2015-12-07 09:45:24 -0500

    12/7/15/09:45

---
+++ Levels.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Levels
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnQuit
+        '
+        Me.btnQuit.BackColor = System.Drawing.Color.Crimson
+        Me.btnQuit.Font = New System.Drawing.Font("Calibri", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuit.ForeColor = System.Drawing.Color.Black
+        Me.btnQuit.Location = New System.Drawing.Point(643, 1)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(173, 46)
+        Me.btnQuit.TabIndex = 5
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = False
+        '
+        'Levels
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.ClientSize = New System.Drawing.Size(817, 528)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Name = "Levels"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnQuit As Button
+End Class
---
+++ Levels.vb
@@ -0,0 +1,9 @@
+﻿Public Class Levels
+    Private Sub Levels_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        End
+    End Sub
+End Class
